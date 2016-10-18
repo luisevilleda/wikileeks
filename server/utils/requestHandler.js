cc21f625ea7473412ca25fed4b299046c614b645
Luis Villeda <luisevilleda@gmail.com>
2016-10-18 13:50:14 -0700

    Configure Heroku env vaiable access again

--- server/utils/requestHandler.js
+++ server/utils/requestHandler.js
@@ -12,7 +12,7 @@ module.exports = {
   analyzeRecipe: function(recipe, cb) {
     request({
       method: 'POST',
-      url: `https://api.edamam.com/api/nutrition-details?app_id=${keys.app_id || process.ENV['S3_ID']}&app_key=${keys.app_key || process.ENV['S3_KEY']}`,
+      url: `https://api.edamam.com/api/nutrition-details?app_id=${keys.app_id || process.env.S3_ID}&app_key=${keys.app_key || process.env.S3_KEY}`,
       headers: {
         'content-type': 'application/json',
       },
