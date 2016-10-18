b625c9452d2fd2f7dac90103e5f4a797211dece1
Luis Villeda <luisevilleda@gmail.com>
2016-10-17 23:27:07 -0700

    Add directory for factories

---
+++ app/factories/makePostRequest.js
@@ -0,0 +1,30 @@
+var app = angular.module('wikiLeeks');
+
+app.factory('makePostRequest', function($http) {
+
+  var factory = {};
+
+  factory.makePostRequest = function(recipe) {
+    console.log('POSTING');
+    $http({
+      method: 'POST',
+      url: '/analyzeRecipe',
+      headers: {
+         'Content-Type': 'application/json'
+       },
+       data: JSON.stringify(recipe)
+    }).then(function successCallback(response) {
+        currentRecipe = response;
+        console.log(response);
+      }, function errorCallback(response) {
+        console.log('Request failed');
+      });
+  };
+
+  factory.getCurrentRecipe = function() {
+    return currentRecipe;
+  };
+
+
+  return factory;
+});
