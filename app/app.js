36c446199a621e2aece82bc5147c34fdb3542957
Luis Villeda <luisevilleda@gmail.com>
2016-10-17 23:20:14 -0700

    Add angular route for viewing nutritional info

--- app/app.js
+++ app/app.js
@@ -6,6 +6,10 @@ app.config(function($routeProvider) {
     templateUrl: 'views/homepage.html',
     controller: 'homepageController'
   })
+  .when('/nutrition', {
+    templateUrl: 'views/nutritionView.html',
+    controller: 'nutritionController'
+  })
   .otherwise({
     templateUrl: 'views/homepage.html',
     controller: 'homepageController'
