3bc71bd9402d807a70aecc70d7eac3e2a43a8a8b
Luis Villeda <luisevilleda@gmail.com>
2016-10-18 18:04:53 -0700

    Modify dbHandler for Heroku

--- server/utils/dbHandler.js
+++ server/utils/dbHandler.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/27017');
+mongoose.connect(`${ process.env.MONGOLAB_URI || 'mongodb://localhost/27017'}`);
 
 var Recipe = mongoose.model('Recipe', {
   title: {type: String, required: true},
