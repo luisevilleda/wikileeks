813a928f00c2f0958ddc9a4a5bbaf7196524f550
Luis Villeda <luisevilleda@gmail.com>
2016-10-18 17:55:56 -0700

    Add route for accessing all recipes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
 var path = require('path');
 
+var db = require('./utils/dbHandler');
 var requestHandler = require('./utils/requestHandler');
 
 var app = express();
@@ -21,10 +22,23 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/../app/index.html');
 });
 
+app.get('/recipes', function(req, res) {
+  // res.sendFile(path.resolve(__dirname + '/../body.example.json'));
+  db.getAllRecipes(function(data) {
+      res.send(data);
+  });
+
+});
+
 app.post('/analyzeRecipe', function(req, res) {
+  var entry = db.initializeEntry(req.body);
   requestHandler.analyzeRecipe(req.body, function(body) {
+    db.addNutrients(entry, body);
     res.send(body);
-  });  
+  });
+  // console.log(entry);
+  // db.addNutrients(entry, {n:"NUTRIENT WILL GO HERE"});
+
   // res.sendFile(path.resolve(__dirname + '/../body.example.json'));
 });
 
